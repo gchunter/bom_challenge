7232ace626485c6aaf1fe28721e574fe98505c5c
gchunter <61396043+gchunter@users.noreply.github.com>
2020-03-17 10:27:59 +1100

    project start 2

--- bom_challenge.R
+++ bom_challenge.R
@@ -3,6 +3,4 @@ library(tidyverse)
 bom_stations <- read_csv("data/BOM_stations.csv")
 view(bom_stations)
 view(bom_data)
-#For each station, how many days have a minimum
-#temperature, a maximum temperature and rainfall
-#measurement recorded?
+?
